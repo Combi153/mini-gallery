cad4c457220130a027871dc0c8f355a36e2d340f
Combi153 <jcm04@naver.com>
2023-10-03 14:41:13 +0900

    [feat] set project

---
+++ src/main/java/huchu/huhaha/HuhahaApplication.java
@@ -0,0 +1,13 @@
+package huchu.huhaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuhahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuhahaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/huchu/huhaha/HuhahaApplicationTests.java
@@ -0,0 +1,13 @@
+package huchu.huhaha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuhahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
