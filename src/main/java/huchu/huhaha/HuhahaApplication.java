344bde00784a5160a7e689c186dcae6d2d834e85
Combi153 <jcm04@naver.com>
2023-10-03 14:44:16 +0900

    [build] 의존성 정리

--- src/main/java/huchu/huhaha/HuhahaApplication.java
+++ src/main/java/huchu/huhaha/HuhahaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HuhahaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HuhahaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HuhahaApplication.class, args);
+    }
 }
