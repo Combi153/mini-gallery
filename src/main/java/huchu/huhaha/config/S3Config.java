342ec955aea52163760cfbe9cecc2a3f29060803
Combi153 <jcm04@naver.com>
2023-10-05 20:55:08 +0900

    [refactor] s3 confing 수정

--- src/main/java/huchu/huhaha/config/S3Config.java
+++ src/main/java/huchu/huhaha/config/S3Config.java
@@ -2,7 +2,7 @@ package huchu.huhaha.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,9 +25,9 @@ public class S3Config {
     }
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3() {
         BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
                 .build();
